0d1e6d690b5805b12f4892dddfb2602a33cb1178
kirito2056 <hajin_2056@naver.com>
2023-02-07 08:57:14 +0900

    Update jparepository.java

--- backend/src/main/java/com/project/backend/jparepository.java
+++ backend/src/main/java/com/project/backend/jparepository.java
@@ -1,9 +1,10 @@
 package com.project.backend;
 
+import jakarta.persistence.criteria.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface jparepository extends JpaRepository<> {
+public interface jparepository extends JpaRepository<Order, Long> {
 
 }
